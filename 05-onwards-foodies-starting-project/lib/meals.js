f9c213fd471c071d3e9b216cb63b55d2252a4a82
dhk939393 <dhk9309@gmail.com>
2025-07-02 15:33:55 +0900

    폼제출 404 페이지 추가

--- 05-onwards-foodies-starting-project/lib/meals.js
+++ 05-onwards-foodies-starting-project/lib/meals.js
@@ -4,6 +4,10 @@ const db = sql('meals.db');
 
 export async function getMeals() {
     await new Promise((resolve) => setTimeout(resolve, 2000)); // Simulate a delay for demonstration purposes
-    throw new Error('Database connection failed'); // Simulate a database error
+    // throw new Error('Database connection failed'); // Simulate a database error
     return db.prepare('SELECT * FROM meals').all();
+}
+
+export function getMeal(slug) {
+    return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug);
 }
